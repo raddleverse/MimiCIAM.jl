9a47c0c98a42e8dac4f35e1c633aa9f526bf718f
Catherine Ledna <cledna@berkeley.edu>
2019-11-25 17:19:40 -0800

    Created function wrappers for initialization

--- src/ciam.jl
+++ src/ciam.jl
@@ -7,42 +7,60 @@ export m
 include("slrcost.jl")
 include("ciamhelper.jl")
 
-# To do wrap in function 
-# Load initialization parameters: run name, lsl source, segments to run, SSP, discountrate 
-d = init()
-run_name = d["run_name"]
-modelparams = import_model_data(d["lslr"][1],d["subset"][1])
-params = modelparams[1]
-xsc = modelparams[2]
-discountrate = 0.04#parse(Float64,d["discountrate"])
+function buildciam(m::Model)
+    add_comp!(m, slrcost)
+    return m
 
-# Dynamically find indices corresponding to USA and CAN and manually set time steps 
-rgn_ind_canada = [k for (k,v) in xsc[5] if v=="CAN"][1]
-rgn_ind_usa = [k for (k,v) in xsc[5] if v=="USA"][1]
-t=10 # Running 10 periods (thru 2100) # To do - make an input argument
-
-# Set model dimensions 
-m = Model()
-set_dimension!(m, :time, t)
-set_dimension!(m, :adaptPers, 1)  # Todo figure out way not to hardcode these
-set_dimension!(m, :regions, xsc[2])
-set_dimension!(m, :segments, xsc[3])
-
-# Add component: slrcost and set some parameters manually 
-add_comp!(m, slrcost)
-set_param!(m, :slrcost, :xsc, xsc[1])
-set_param!(m, :slrcost, :rgn_ind_canada, rgn_ind_canada)
-set_param!(m, :slrcost, :rgn_ind_usa, rgn_ind_usa)
-set_param!(m, :slrcost, :discountrate, discountrate)
-
-# Shorten some time-dependent parameters to correspond to the correct number of timesteps 
-for k in keys(params)
-    if size(params[k])!=() && length(size(params[k]))==2 && size(params[k])[1]>t && k!="surgeexposure"
-        params[k]=params[k][1:t,:]
-    end
 end
 
-# Set the rest of the parameters 
-set_leftover_params!(m, params)
+function initciam(xsc, params, m::Model, num_pers::Int=20)
+
+    discountrate = 0.04#parse(Float64,d["discountrate"])
+    
+    # Dynamically find indices corresponding to USA and CAN and manually set time steps 
+    rgn_ind_canada = [k for (k,v) in xsc[5] if v=="CAN"][1]
+    rgn_ind_usa = [k for (k,v) in xsc[5] if v=="USA"][1]
+
+    # Add component: slrcost and set some parameters manually 
+    set_param!(m, :slrcost, :xsc, xsc[1])
+    set_param!(m, :slrcost, :rgn_ind_canada, rgn_ind_canada)
+    set_param!(m, :slrcost, :rgn_ind_usa, rgn_ind_usa)
+    set_param!(m, :slrcost, :discountrate, discountrate)
+
+    # Shorten some time-dependent parameters to correspond to the correct number of timesteps 
+    for k in keys(params)
+        if size(params[k])!=() && length(size(params[k]))==2 && size(params[k])[1]>num_pers && k!="surgeexposure"
+            params[k]=params[k][1:num_pers,:]
+        end
+    end
+
+    # Set the rest of the parameters 
+    set_leftover_params!(m, params)
+
+end
+
+function get_model(num_pers::Int=20)
+    d = init()
+    run_name = d["run_name"]
+
+    modelparams = import_model_data(d["lslr"][1],d["subset"][1])
+    params = modelparams[1]
+    xsc = modelparams[2]
+    
+    m=Model()
+
+    set_dimension!(m, :time, num_pers)
+    set_dimension!(m, :adaptPers, 1)  # Todo figure out way not to hardcode these TODO change to default adapt periods 
+    set_dimension!(m, :regions, xsc[2])
+    set_dimension!(m, :segments, xsc[3])
+
+    buildciam(m)
+    initciam(xsc, params, m, num_pers)
+
+    return m 
+
+end
+
+getciam=get_model 
 
 end
