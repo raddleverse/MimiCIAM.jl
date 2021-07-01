6af785fcf3eed0dbed61d661caddc9cdcd2d379e
lrennels <lrennels@berkeley.edu>
2021-06-30 18:25:06 -0700

    Clean up files, add comments, fix packageification

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Mimi-CIAM.jl
+# MimiCIAM.jl
+
+This is a work-in-progress respository for a Julia-Mimi implementation the CIAM model adapted from Diaz, 2016.
+
+## Preparing the Software Environment
+
+Your first step is to install MimiCIAM.jl itself, and to do so you need to run the following command at the julia package REPL:
+
+```julia
+pkg> add https://github.com/BRICK-SLR/MimiCIAM.jl.git
+```
+
+You probably also want to install the Mimi package into your julia environment, so that you can use some of the tools in there:
+
+```julia
+pkg> add Mimi
+```
+## Running the Model
+
+The model uses the Mimi framework and it is highly recommended to read the Mimi  documentation first to understand the code structure. The basic way to access a copy of the default MimiFAIRv2 model and explore the resuts is the following:
+
+The basic way to access a copy of the default MimiCIAM model is the following:
+
+```julia
+using MimiCIAM
+m = MimiCIAM.get_model()
+run(m)
+```
+
+The get_model() function currently has the following keyword arguments:
+- `initfile`: (default = nothing)
+- `fixed`: (default = false)
+- `noRetreat`: (default = false)
+- `allowMaintain`: (default = true)
+- `popinput`: (default = 0)
--- src/ciam.jl
+++ src/ciam.jl
@@ -1,23 +1,25 @@
+using Mimi
 
-function buildciam(m::Model)
-    add_comp!(m, slrcost)
-    return m
+"""
+    initciam(xsc, params, initparams, m::Model; fixed::Bool = false, t::Int = 20, 
+            noRetreat::Bool = false, allowMaintain::Bool = true, popinput::Int = 0)
 
-end
+Initialize a CIAM model `m` with the given arguments.
+"""
+function initciam(xsc, params, initparams, m::Model; fixed::Bool = false, t::Int = 20, 
+                noRetreat::Bool = false, allowMaintain::Bool = true, popinput::Int = 0)
 
-function initciam(xsc, params, initparams, m::Model, fixed::Bool=false,t::Int=20,noRetreat::Bool=false,allowMaintain::Bool=true,popinput::Int=0)
+    discountrate = 0.04
 
-    discountrate = 0.04#parse(Float64,d["discountrate"])
     if initparams["lslr"][1] !="lsl_rcp0_p50.csv"
         rcp = parse(Int64,replace(replace(initparams["lslr"][1],r"^[^l]*lsl_rcp"=>s""),r"_.*"=>s""))
         pctl = parse(Int64,replace(replace(initparams["lslr"][1], r"^[^l]*lsl_rcp[0-9][0-9]_p"=>s""),r".csv"=>s""))
     else
-        rcp=0
-        pctl=50
+        rcp = 0
+        pctl = 50
     end
 
-    if initparams["ssp"][1]==false
-    #if initparams["ssp"]==false
+    if initparams["ssp"][1] == false
         ssp=0
     else
         ssp = parse(Int64,replace(replace(initparams["ssp"][1],r"^[^l]*SSP"=>s""),r"_.*"=>s""))
@@ -26,69 +28,75 @@ function initciam(xsc, params, initparams, m::Model, fixed::Bool=false,t::Int=20
     # Dynamically find indices corresponding to USA and CAN and manually set time steps
     # If the lengths are 0, then assume those segments are not used. Note that
     # if including Greenland, need Canada too as a reference for land appreciation
+    
     rgn_ind_canada = [k for (k,v) in xsc[4] if v=="CAN"]
-    if length(rgn_ind_canada) > 0
-        rgn_ind_canada = rgn_ind_canada[1]
-    else
-        rgn_ind_canada = 0
-    end
+    rgn_ind_canada = (length(rgn_ind_canada) > 0) ? rgn_ind_canada[1] : 0
+
     rgn_ind_usa = [k for (k,v) in xsc[4] if v=="USA"]
-    if length(rgn_ind_usa) > 0
-        rgn_ind_usa = rgn_ind_usa[1]
-    else
-        rgn_ind_usa = 0
-    end
+    rgn_ind_usa = (length(rgn_ind_usa) > 0) ? rgn_ind_usa[1] : 0
 
     # Add component: slrcost and set some parameters manually
     segID = segStr_to_segID(xsc[3])
-    set_param!(m, :slrcost, :segID, segID)
-    set_param!(m, :slrcost, :rcp, rcp)
-    set_param!(m, :slrcost, :percentile, pctl)
-    set_param!(m, :slrcost, :ssp, ssp)
-    set_param!(m, :slrcost, :xsc, xsc[1])
-    set_param!(m, :slrcost, :rgn_ind_canada, rgn_ind_canada)
-    set_param!(m, :slrcost, :rgn_ind_usa, rgn_ind_usa)
-    set_param!(m, :slrcost, :discountrate, discountrate)
-    set_param!(m, :slrcost, :ntsteps, t)
-    set_param!(m, :slrcost, :fixed, fixed)
-    set_param!(m, :slrcost, :noRetreat, noRetreat)
-    set_param!(m, :slrcost, :allowMaintain, allowMaintain)
-    set_param!(m, :slrcost, :popinput, popinput)
+    
+    update_param!(m, :slrcost, :segID, segID)
+    update_param!(m, :slrcost, :rcp, rcp)
+    update_param!(m, :slrcost, :percentile, pctl)
+    update_param!(m, :slrcost, :ssp, ssp)
+    update_param!(m, :slrcost, :xsc, xsc[1])
+    update_param!(m, :slrcost, :rgn_ind_canada, rgn_ind_canada)
+    update_param!(m, :slrcost, :rgn_ind_usa, rgn_ind_usa)
+    update_param!(m, :slrcost, :discountrate, discountrate)
+    update_param!(m, :slrcost, :ntsteps, t)
+    update_param!(m, :slrcost, :fixed, fixed)
+    update_param!(m, :slrcost, :noRetreat, noRetreat)
+    update_param!(m, :slrcost, :allowMaintain, allowMaintain)
+    update_param!(m, :slrcost, :popinput, popinput)
 
     # Shorten some time-dependent parameters to correspond to the correct number of timesteps
     for k in keys(params)
-        if size(params[k])!=() && length(size(params[k]))==2 && size(params[k])[1]>t && k!="surgeexposure"
-            params[k]=params[k][1:t,:]
+        if ndims(params[k]) == 0 && ndims(params[k]) == 2 && size(params[k])[1] > t && k != "surgeexposure"
+            params[k] = params[k][1:t,:]
         end
     end
 
-    # Set the rest of the parameters
-    set_leftover_params!(m, params)
+    # Set the rest of the parameters - to use update_leftover_params! we need To
+    # specific the component as well as the parameter name
+    params_dict = Dict()
+    for (k,v) in params
+        params_dict[(:slrcost, k)] = v
+    end
+    update_leftover_params!(m, params_dict)
 
 end
 
-function get_model(;initfile=nothing,fixed::Bool=false,t::Int=20,noRetreat::Bool=false,allowMaintain::Bool=true, popinput::Int=0)
-    initparams= init(initfile)
-    modelparams = import_model_data(initparams["lslr"][1],initparams["subset"][1],initparams["ssp"][1],initparams["ssp_simplified"][1])
-    params = modelparams[1]
-    xsc = modelparams[2]
+"""
+    get_model(;initfile::Union{String, Nothing} = nothing, fixed::Bool=false, 
+                t::Int = 20, noRetreat::Bool = false, allowMaintain::Bool = true, 
+                popinput::Int = 0)
+Return a initialized and built CIAM model with the given arguments.
+"""
+function get_model(;initfile::Union{String, Nothing} = nothing, fixed::Bool=false, 
+                    t::Int = 20, noRetreat::Bool = false, allowMaintain::Bool = true, 
+                    popinput::Int = 0)
 
-    m=Model()
+    initparams  = init(; f = initfile)
+    params, xsc = import_model_data(initparams["lslr"][1], initparams["subset"][1], initparams["ssp"][1], initparams["ssp_simplified"][1])
+
+    m = Model()
 
     set_dimension!(m, :time, t)
     set_dimension!(m, :adaptPers, length(params["at"]))
     set_dimension!(m, :regions, xsc[2])
     set_dimension!(m, :segments, xsc[3])
 
-    buildciam(m)
+    add_comp!(m, slrcost)
 
-    initciam(xsc, params, initparams, m, fixed, t,noRetreat,allowMaintain, popinput)
+    initciam(xsc, params, initparams, m; fixed = fixed, t = t, noRetreat = noRetreat, allowMaintain = allowMaintain, popinput = popinput)
 
     return m
 
 end
 
-# Code to run a batch instance of the model
-function update_model(updateparams,updatevalues)
-
-end
+# TODO - Code to run a batch instance of the model
+# function update_model(updateparams,updatevalues)
+# end
