e2fc7f5fad3277287aafe9756c20a3a7b0c3a791
lrennels <lrennels@berkeley.edu>
2021-07-01 17:07:02 -0700

    More formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,5 @@ using MimiCIAM
 using Test
 
 include("utils.jl")
-
-run_baseline_comparisons = true # slow so only include it when have the time etc.
-
 include("test_unit_tests.jl")
-run_baseline_comparisons && include("test_baseline_comparisons.jl")
+# include("test_baseline_comparisons.jl") # slow so only include it when have the time etc.
