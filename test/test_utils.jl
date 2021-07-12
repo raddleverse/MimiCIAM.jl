32b174bcfb0f51c34bcaea9833242adb054c9a2e
Tony Wong <aewsma@cos-math2004.main.ad.rit.edu>
2021-07-11 23:36:36 -0400

    removing Jones runs from the validation data

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -8,8 +8,8 @@ function write_MimiCIAM_comparison_files(outputdir; subset = false)
     isdir(outputdir) || mkpath(outputdir)
 
     ##==============================================================================
-    ## ctrl+noConstrFix: This case is run with a modified slrcost component held in 
-    ## slrcost_GAMSmatch.jl, which is taken care of in the `get_model` step with the 
+    ## ctrl+noConstrFix: This case is run with a modified slrcost component held in
+    ## slrcost_GAMSmatch.jl, which is taken care of in the `get_model` step with the
     ## GAMS match arg and removes the block that disallows height reductions
 
     run_name = "ctrl+noConstrFix"
@@ -37,7 +37,7 @@ function write_MimiCIAM_comparison_files(outputdir; subset = false)
     m = MimiCIAM.get_model(
         initfile        = joinpath(outputdir, init_settings[:init_filename]),
         fixed           = model_settings[:fixed],
-        t               = model_settings[:t], 
+        t               = model_settings[:t],
         noRetreat       = model_settings[:noRetreat],
         allowMaintain   = model_settings[:allowMaintain],
         popinput        = model_settings[:popinput],
@@ -74,7 +74,7 @@ function write_MimiCIAM_comparison_files(outputdir; subset = false)
     m = MimiCIAM.get_model(
         initfile        = joinpath(outputdir, init_settings[:init_filename]),
         fixed           = model_settings[:fixed],
-        t               = model_settings[:t], 
+        t               = model_settings[:t],
         noRetreat       = model_settings[:noRetreat],
         allowMaintain   = model_settings[:allowMaintain],
         popinput        = model_settings[:popinput],
@@ -111,7 +111,7 @@ function write_MimiCIAM_comparison_files(outputdir; subset = false)
     m = MimiCIAM.get_model(
         initfile        = joinpath(outputdir, init_settings[:init_filename]),
         fixed           = model_settings[:fixed],
-        t               = model_settings[:t], 
+        t               = model_settings[:t],
         noRetreat       = model_settings[:noRetreat],
         allowMaintain   = model_settings[:allowMaintain],
         popinput        = model_settings[:popinput],
@@ -121,40 +121,4 @@ function write_MimiCIAM_comparison_files(outputdir; subset = false)
 
     MimiCIAM.write_output_files(m, outputdir, run_name)
 
-    ##==============================================================================
-    ##  baseline+updated population density from Jones and O'Neill (2016)
-
-    run_name = "ctrl+SSP5+popJones"
-
-    init_settings = Dict(
-        :init_filename   => string("$run_name", "_init.csv"),
-        :lslrfile        => "lsl_rcp85_p50.csv",
-        :subset          => subset,
-        :ssp             => "IIASAGDP_SSP5_v9_130219",
-        :ssp_simplified  => "5" # based on SSPs, so need to use an SSP
-    )
-
-    model_settings = Dict(
-        :fixed          => true,
-        :t              => 15,
-        :noRetreat      => false,
-        :allowMaintain  => false,
-        :popinput       => 1, # 0 = default old stuff, 1 = Jones and O'Neill (2016), 2 = Merkens et al (2016)
-        :GAMSmatch      => false
-    )
-
-    MimiCIAM.write_init_file(run_name, outputdir, init_settings)
-
-    m = MimiCIAM.get_model(
-        initfile        = joinpath(outputdir, init_settings[:init_filename]),
-        fixed           = model_settings[:fixed],
-        t               = model_settings[:t], 
-        noRetreat       = model_settings[:noRetreat],
-        allowMaintain   = model_settings[:allowMaintain],
-        popinput        = model_settings[:popinput],
-        GAMSmatch       = model_settings[:GAMSmatch]
-    )
-    run(m)
-
-    MimiCIAM.write_output_files(m, outputdir, run_name)
 end
